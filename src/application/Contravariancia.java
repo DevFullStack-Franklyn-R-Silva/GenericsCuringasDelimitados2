b31078dc351e715d88d9274bbb596a269b70b0f3
Franklyn <franklyn1998@live.com>
2021-09-28 14:21:37 -0300

    Projeto criado

---
+++ src/application/Contravariancia.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contravariancia {
+
+	public static void main(String[] args) {
+
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+
+		List<? super Number> myNums = myObjs;
+		myNums.add(10);
+		myNums.add(3.14);
+		// Number x = myNums.get(0); // erro de compilacao
+	}
+}
